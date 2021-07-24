46ce1cca83b9c8548984849d04dbd569b503735b
romajc <joeyroman84@gmail.com>
2021-07-24 15:57:01 -0400

    working before adding ticker

--- lib/services.dart
+++ lib/services.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 import 'package:practice_add_to_list/stock_quote_model.dart';
 
 class StockQuoteServices {
-  static Future<StockQuote?> getStockQuote(String ticker) async {
+  static Future<StockQuote?> getStockQuote() async {
     String url =
-        'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=$ticker&apikey=XQQW1OPQ70ZODVES';
+        'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=sq&apikey=XQQW1OPQ70ZODVES';
 
     http.Response response;
     response = await http.get(Uri.parse(url));
